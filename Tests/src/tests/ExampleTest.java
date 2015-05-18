909b8723074c55151d1855374c6cfe9e9b4c7c98
Alex <sonicdude377@hotmail.com>
2015-05-17 22:11:24 -0400

    Started PhotoBooks tests in a seperate project so that test libraries aren't bundled with release application.

---
+++ Tests/src/tests/ExampleTest.java
@@ -0,0 +1,20 @@
+package tests;
+
+import junit.framework.TestCase;
+
+public class ExampleTest extends TestCase
+{
+	public ExampleTest(String arg0) {
+		super(arg0);
+	}
+	
+	public void testExampleSucceed()
+	{
+		assertTrue(true);
+	}
+	
+	public void testExampleFail()
+	{
+		assertTrue(false);
+	}
+}
---
+++ Tests/src/tests/UnitTests.java
@@ -0,0 +1,23 @@
+package tests;
+
+import junit.framework.TestSuite;
+import junit.framework.Test;
+
+public class UnitTests {
+	
+	public static TestSuite suite;
+	
+	public static Test suite()
+	{
+		suite = new TestSuite("Unit Tests");
+		
+		testObjects();
+		
+		return suite;
+	}
+	
+	private static void testObjects()
+	{
+		suite.addTestSuite(ExampleTest.class);
+	}
+}
