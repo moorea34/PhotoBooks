bb071858ee0a585e7d9b5fcab0309baf5ef4c528
Alex <sonicdude377@hotmail.com>
2015-06-10 21:27:07 -0500

    Tested dao table initilizers.

--- Tests/src/tests/businessManagerTests/ClientManagerTest.java
+++ Tests/src/tests/businessManagerTests/ClientManagerTest.java
@@ -1,11 +1,28 @@
 package tests.businessManagerTests;
 
+import photobooks.business2.ClientManager;
+import photobooks.gateways2.IDao;
+import photobooks.hsqldbgateways.HSQLDBDao;
 import junit.framework.TestCase;
 
 public class ClientManagerTest extends TestCase {
 	
-	public ClientManagerTest(String arg0)
-	{
+	public ClientManagerTest(String arg0) {
 		super(arg0);
+	}
+	
+	public void testClientManager() {
+		IDao _dao = null;
+		ClientManager _clientManager = null;
+		
+		HSQLDBDao dao = new HSQLDBDao("Test");
+		dao.initialize();
+		dao.commitChanges();
+		
+		_dao = dao;
+		_clientManager = new ClientManager(_dao);
+		
+		_dao.rollback();
+		_dao.dispose();
 	}
 }
